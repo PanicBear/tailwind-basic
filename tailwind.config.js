6f62831f029e49cb2fd38b67f15c39310518fa14
ClarXo <imp_rove@naver.com>
2022-02-07 15:34:44 +0900

    modifier : dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [],
 };
